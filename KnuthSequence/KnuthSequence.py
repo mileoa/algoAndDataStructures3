5beae1b65a5ee83086636a5c6dd242ecca58bbd4
mileoa <ezkimozzzzz@gmail.com>
2024-10-28 21:45:02 +1000

    fix: correct logic

--- KnuthSequence/KnuthSequence.py
+++ KnuthSequence/KnuthSequence.py
@@ -3,10 +3,12 @@ from typing import List
 
 def KnuthSequence(array_size: int) -> List[int]:
     if array_size == 0:
+        return []
+    if array_size == 1:
         return [1]
     sequence: List[int] = []
     n = 1
-    while array_size >= n:
+    while array_size > n:
         sequence.append(n)
         n = n * 3 + 1
     sequence.reverse()
--- KnuthSequence/test_KnuthSequence.py
+++ KnuthSequence/test_KnuthSequence.py
@@ -9,11 +9,11 @@ class KnuthSequenceTests(unittest.TestCase):
     def test_regression(self):
         self.assertEqual(KnuthSequence(15), [13, 4, 1])
         self.assertEqual(KnuthSequence(15), [13, 4, 1])
-        self.assertEqual(KnuthSequence(0), [1])
+        self.assertEqual(KnuthSequence(0), [])
         self.assertEqual(KnuthSequence(1), [1])
         self.assertEqual(KnuthSequence(2), [1])
         self.assertEqual(KnuthSequence(5), [4, 1])
-        self.assertEqual(KnuthSequence(13), [13, 4, 1])
+        self.assertEqual(KnuthSequence(13), [4, 1])
         self.assertEqual(KnuthSequence(365), [364, 121, 40, 13, 4, 1])
 
 
