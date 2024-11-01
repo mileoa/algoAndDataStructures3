b5efbb806d25852f6d4b6a86b2f2b51419184d39
mileoa <ezkimozzzzz@gmail.com>
2024-11-02 00:26:01 +1000

    feature: add tests

--- QuickSort/test_QuickSort.py
+++ QuickSort/test_QuickSort.py
@@ -8,9 +8,13 @@ from QuickSort import QuickSort
 class QuickSortTests(unittest.TestCase):
 
     def test_regression(self) -> None:
-        array: List[int] = [5, 2, 9, 1, 3, 6, 7]
-        QuickSort(array, 0, len(array) - 1)
-        self.assertEqual(array, [1, 2, 3, 5, 6, 7, 9])
+        first_array: List[int] = [5, 2, 9, 1, 3, 6, 7]
+        QuickSort(first_array, 0, len(first_array) - 1)
+        self.assertEqual(first_array, [1, 2, 3, 5, 6, 7, 9])
+
+        second_array: List[int] = [5, 2, 9, 1, 3, 6, 7]
+        QuickSort(second_array, 2, 5)
+        self.assertEqual(second_array, [5, 2, 1, 3, 6, 9, 7])
 
     def test_empty(self) -> None:
         empty_array: List[int] = []
