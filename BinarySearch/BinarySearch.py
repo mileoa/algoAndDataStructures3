a1973ae567fedb688c94d44f2a3b0702286c0ea6
mileoa <ezkimozzzzz@gmail.com>
2024-11-14 19:25:06 +1000

    feat: BinarySearch

---
+++ BinarySearch/BinarySearch.py
@@ -0,0 +1,29 @@
+from typing import List, Optional
+
+
+class BinarySearch:
+
+    def __init__(self, sorted_array: List[int]) -> None:
+        self.sorted_array: List[int] = sorted_array
+        self.Left: int = 0
+        self.Right: int = len(sorted_array) - 1
+        self.search_result: int = 0
+
+    def Step(self, N: int) -> None:
+        if self.search_result != 0:
+            return None
+        mid: int = (self.Left + self.Right) // 2
+        if self.sorted_array[mid] == N:
+            self.search_result = 1
+            return None
+        if self.sorted_array[mid] < N:
+            self.Left = mid + 1
+        if self.sorted_array[mid] > N:
+            self.Right = mid - 1
+        if self.Left - self.Right == 1:
+            self.search_result = -1
+            return None
+        return None
+
+    def GetResult(self) -> int:
+        return self.search_result
---
+++ BinarySearch/test_BinarySearch.py
@@ -0,0 +1,75 @@
+import unittest
+import random
+
+from BinarySearch import BinarySearch
+
+
+class BinarySearchTests(unittest.TestCase):
+
+    def test_regression_Step_and_GetResult(self):
+        bs_one = BinarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        bs_one.Step(3)
+        self.assertEqual(bs_one.Left, 0)
+        self.assertEqual(bs_one.Right, 3)
+        self.assertEqual(bs_one.GetResult(), 0)
+
+        bs_one.Step(3)
+        self.assertEqual(bs_one.Left, 2)
+        self.assertEqual(bs_one.Right, 3)
+        self.assertEqual(bs_one.GetResult(), 0)
+
+        bs_one.Step(3)
+        self.assertEqual(bs_one.Left, 2)
+        self.assertEqual(bs_one.Right, 3)
+        self.assertEqual(bs_one.GetResult(), 1)
+
+        bs_one.Step(3)
+        self.assertEqual(bs_one.Left, 2)
+        self.assertEqual(bs_one.Right, 3)
+        self.assertEqual(bs_one.GetResult(), 1)
+
+        bs_two = BinarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        bs_two.Step(10)
+        self.assertEqual(bs_two.Left, 5)
+        self.assertEqual(bs_two.Right, 8)
+        self.assertEqual(bs_two.GetResult(), 0)
+
+        bs_two.Step(10)
+        self.assertEqual(bs_two.Left, 7)
+        self.assertEqual(bs_two.Right, 8)
+        self.assertEqual(bs_two.GetResult(), 0)
+
+        bs_two.Step(10)
+        self.assertEqual(bs_two.Left, 8)
+        self.assertEqual(bs_two.Right, 8)
+        self.assertEqual(bs_two.GetResult(), 0)
+
+        bs_two.Step(10)
+        self.assertEqual(bs_two.Left, 9)
+        self.assertEqual(bs_two.Right, 8)
+        self.assertEqual(bs_two.GetResult(), -1)
+
+        bs_three = BinarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9])
+        bs_three.Step(9)
+        self.assertEqual(bs_three.Left, 5)
+        self.assertEqual(bs_three.Right, 8)
+        self.assertEqual(bs_three.GetResult(), 0)
+
+        bs_three.Step(9)
+        self.assertEqual(bs_three.Left, 7)
+        self.assertEqual(bs_three.Right, 8)
+        self.assertEqual(bs_three.GetResult(), 0)
+
+        bs_three.Step(9)
+        self.assertEqual(bs_three.Left, 8)
+        self.assertEqual(bs_three.Right, 8)
+        self.assertEqual(bs_three.GetResult(), 0)
+
+        bs_three.Step(9)
+        self.assertEqual(bs_three.Left, 8)
+        self.assertEqual(bs_three.Right, 8)
+        self.assertEqual(bs_three.GetResult(), 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
