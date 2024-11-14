f9db8991a146d7bdfef5396a26c3a01be786177e
mileoa <ezkimozzzzz@gmail.com>
2024-11-14 21:19:02 +1000

    fix: retuen type and type annotation

--- BinarySearch/BinarySearch.py
+++ BinarySearch/BinarySearch.py
@@ -7,15 +7,15 @@ class BinarySearch:
         self.sorted_array: List[int] = sorted_array
         self.Left: int = 0
         self.Right: int = len(sorted_array) - 1
-        self.search_result: str = "0"
+        self.search_result: int = 0
 
     def Step(self, N: int) -> None:
-        if self.search_result != "0":
+        if self.search_result != 0:
             return None
         mid: int = (self.Left + self.Right) // 2
 
         if self.sorted_array[mid] == N:
-            self.search_result = "+1"
+            self.search_result = +1
             return None
 
         if self.sorted_array[mid] < N:
@@ -25,17 +25,17 @@ class BinarySearch:
 
         if self.Left == self.Right:
             if self.sorted_array[self.Left] == N:
-                self.search_result = "+1"
+                self.search_result = +1
             if self.sorted_array[self.Left] != N:
-                self.search_result = "-1"
+                self.search_result = -1
             return None
 
         if abs(self.Left - self.Right) == 1:
             if N in (self.sorted_array[self.Left], self.sorted_array[self.Right]):
-                self.search_result = "+1"
+                self.search_result = 1
                 return None
-            self.search_result = "-1"
+            self.search_result = -1
             return None
 
-    def GetResult(self) -> str:
+    def GetResult(self) -> int:
         return self.search_result
